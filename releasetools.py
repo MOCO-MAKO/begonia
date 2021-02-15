5e788e6a6455c485f51b6a64acb9456921da81ba
SahilSonar <sss.sonar2003@gmail.com>
2021-02-15 05:08:24 +0100

    [DNM] [WIP] begonia: Pack required firmware into the final potato build

--- releasetools.py
+++ releasetools.py
@@ -19,9 +19,11 @@ import re
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info, False)
+  Firmware_Images(info, False)
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info, True)
+  Firmware_Images(info, True)
 
 def AddImage(info, basename, dest, incremental):
   name = basename
@@ -37,3 +39,18 @@ def OTA_InstallEnd(info, incremental):
   info.script.Print("Patching vbmeta and dtbo images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta", incremental)
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo", incremental)
+
+def Firmware_Images(info, incremental):
+    partition_list = ["lk", "lk2", "audio_dsp", "cam_vpu1", "cam_vpu2", "cam_vpu3", "gz1", "gz2", "md1img", "oem_misc1", "scp1", "scp2", "spmfw", "sspm_1", "sspm_2", "tee1", "tee2"]
+    block_list = ["sda", "sdb"]
+    info.script.AppendExtra('ifelse(getprop("ro.boot.hwc") == "India"),'
+                               'package_extract_file("logo_in.bin", "/dev/block/platform/bootdevice/by-name/logo"),'
+                               'package_extract_file("logo.bin", "/dev/block/platform/bootdevice/by-name/logo");')
+    for partition in partition_list:
+        info.script.AppendExtra('ifelse(getprop("ro.boot.hwc") == "India"),'
+                               'package_extract_file("{}_in.img", "/dev/block/platform/bootdevice/by-name/{}");'.format(partition, partition),
+                               'package_extract_file("{}.img", "/dev/block/platform/bootdevice/by-name/{}");'.format(partition, partition))
+    for partition in block_list:
+        info.script.AppendExtra('ifelse(getprop("ro.boot.hwc") == "India"),'
+                               'package_extract_file("{}_in.img", "/dev/block/{}");'.format(partition, partition),
+                               'package_extract_file("{}.img", "/dev/block/{}");'.format(partition, partition))
